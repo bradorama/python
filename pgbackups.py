ad34592cee83498f66a018f13ecc5282d5ef7098
Brad Wallace <swellguy@gmail.com>
2013-12-03 09:16:24 -0800

    Added syslogging and duration.  1.0

--- pgbackups.py
+++ pgbackups.py
@@ -1,8 +1,7 @@
-  GNU nano 2.0.9                                                         File: test.py                                                                                                              Modified
-
 #! /usr/bin/python
 import datetime
 import psycopg2
+import syslog
 from subprocess import call
 # Postgres Backup Script
 # Backs up and encrypts PG databases.
@@ -19,10 +18,14 @@ gpg = '|gpg -o '
 starttime = datetime.datetime.now()
 starttimestr = str(starttime.day) + str(starttime.month) + str(starttime.year) + str(starttime.hour) + str(starttime.minute)
 
+#Execution
+
 try:
     conn = psycopg2.connect("dbname='gold' user='admin' host='postgresql01' password='3e3p0pCs'")
+    syslog.syslog('PG Backup Started')
 except:
     print "I am unable to connect to the database"
+    syslog.syslog.LOG_ERR('PG Backup Script DB Connection Error')
 
 cur = conn.cursor()
 cur.execute("""SELECT datname from pg_database WHERE datistemplate = FALSE""")
@@ -30,6 +33,7 @@ rows = cur.fetchall()
 for row in rows:
     print 'Processsing Database: ' + row[0]
     call('pg_dump ' + row[0] + psql + gpg + backup_dir + row[0] + starttimestr + '.gpg --encrypt --recipient ' + gpgprofile, shell=True)
-endtime = datetime.datetime.now()
-print starttime - endtime
+duration = datetime.datetime.now() - starttime
+syslog.syslog('PG Backup Completed. Duration: ' + str(duration) )
+
 
