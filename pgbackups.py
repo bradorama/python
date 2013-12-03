8033489bd7626bbaad09df12bdb1bd7ea7aee21f
Brad Wallace <swellguy@gmail.com>
2013-12-03 08:31:31 -0800

    Update pgbackups.py
    Documentation and cleanup

--- pgbackups.py
+++ pgbackups.py
@@ -1,13 +1,23 @@
+  GNU nano 2.0.9                                                         File: test.py                                                                                                              Modified
+
 #! /usr/bin/python
 import datetime
 import psycopg2
 from subprocess import call
+# Postgres Backup Script
+# Backs up and encrypts PG databases.
+# Requires passwordless authentication for user. (e.g. .pgpass)
 
+#Definitions
+
+host = 'postgresql01'
+user = 'admin'
+gpgprofile = 'CertainDatabases'
 backup_dir = '/home/python/scripts/'
-psql = ' -U admin -h postgresql01'
+psql = ' -U ' + user + ' -h ' + host
 gpg = '|gpg -o '
-now = datetime.datetime.now()
-datetime = str(now.day) + str(now.month) + str(now.year) + str(now.hour) + str(now.minute)
+starttime = datetime.datetime.now()
+starttimestr = str(starttime.day) + str(starttime.month) + str(starttime.year) + str(starttime.hour) + str(starttime.minute)
 
 try:
     conn = psycopg2.connect("dbname='gold' user='admin' host='postgresql01' password='3e3p0pCs'")
@@ -19,5 +29,7 @@ cur.execute("""SELECT datname from pg_database WHERE datistemplate = FALSE""")
 rows = cur.fetchall()
 for row in rows:
     print 'Processsing Database: ' + row[0]
-    call('pg_dump ' + row[0] + psql + gpg + backup_dir + row[0] + datetime + '.gpg --encrypt --recipient CertainDatabases', shell=True)
+    call('pg_dump ' + row[0] + psql + gpg + backup_dir + row[0] + starttimestr + '.gpg --encrypt --recipient ' + gpgprofile, shell=True)
+endtime = datetime.datetime.now()
+print starttime - endtime
 
