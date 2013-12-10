49a3f3ea9faf2cee45a75f12ccf529a7429a508d
Brad Wallace <swellguy@gmail.com>
2013-12-10 10:20:38 -0800

    1.0 Release

--- liquibased.py
+++ liquibased.py
@@ -1,9 +1,8 @@
-
 #!/usr/bin/python
 # Calls liquibase against a target database
-#
+# 
 # BMW 12/9/2013
-#java -jar liquibase.jar --classpath=/home/liquibase/certain/travis/lib/postgresql-9.3-1100.jdbc4.jar --driver=org.postgresql.Driver  --changeLogFile=/home/liquibase/certain/travis/certain/changelog.xml  -$
+#java -jar liquibase.jar --classpath=/home/liquibase/certain/travis/lib/postgresql-9.3-1100.jdbc4.jar --driver=org.postgresql.Driver  --changeLogFile=/home/liquibase/certain/travis/certain/changelog.xml  --url="jdbc:postgresql://postgresql01/certain"  --username=admin  --password=3e3p0pCs  update
 #
 # Use:  liquibased(host, tag, database)
 #
@@ -15,9 +14,9 @@ from optparse import OptionParser
 #declarations
 java_path = '/home/liquibase/certain/travis/lib/'
 db_driver = '--classpath=/home/liquibase/certain/travis/lib/postgresql-9.3-1100.jdbc4.jar'
-source_path = '--changeLogFile=/home/liquibase/certain/travis/certain/changelog.xml'
+source_path = '--changeLogFile=/home/liquibase/certain/travis/certain/changelog.xml' 
 url='--url jdbc:postgresql://'
-username='admin'
+username='admin'  
 password='3e3p0pCs'
 
 #handle arguments
@@ -28,18 +27,24 @@ parser.add_option("-t",
                   dest="tag",
                   help="tag for liquibase")
 parser.add_option("-d",
-                  dest="dbname",
+		  dest="dbname",
                   help="destination database (omit to process all databases)",
-                  default="alldatabases")
-
+		  default="alldatabases")
+		
 
 (options, args) = parser.parse_args()
 
-url2 = '/' + options.dbname
+#ugly argument handling.  I'm sorry
 
-def liquibased(host, tag):
-        target = url + host + url2
-        command =  'java -jar ' + java_path +'liquibase.jar ' + db_driver + ' ' + source_path + ' ' + target + ' --username ' + username + ' --password ' + password + ' update'
+if options.dbname == 'alldatabases': 
+    url2 = '/' 
+else:
+    url2 = '/' + options.dbname
+
+
+def liquibased(host, tag, database):
+        target = url + host + url2 + database
+        command =  'java -jar ' + java_path +'liquibase.jar ' + db_driver + ' ' + source_path + ' ' + target + ' --username ' + username + ' --password ' + password + ' update' 
 #        print command
         subprocess.check_call(command, shell=True)
         subprocess.check_call(command + ' tag ' + tag, shell=True)
@@ -47,11 +52,26 @@ def liquibased(host, tag):
 
 def getportals(host):
     try:
-        connection_string = "dbname='certain_system' host=\'" +host+ "\' user='" + username + "\' password='" + password + "\'"
-#       conn = psycopg2.connect("dbname='certain_system' user='admin' host='Postgresql01' password='3e3p0pCs'")
+	connection_string = "dbname='certain_system' host=\'" +host+ "\' user='" + username + "\' password='" + password + "\'"
+#	conn = psycopg2.connect("dbname='certain_system' user='admin' host='Postgresql01' password='3e3p0pCs'")
         conn = psycopg2.connect(connection_string)
+        cur = conn.cursor()
+	cur.execute("""SELECT prtl_code FROM portals""")
+	rows = cur.fetchall()
+	return rows
     except Exception, e:
         print e[0]
+        
+
+#print options.host + ' ' + options.tag
+if options.dbname == 'alldatabases':
+    rows = getportals(options.host)
+    for row in rows:
+	print 'Processing: ' + row[0]
+	liquibased(options.host, options.tag, row[0])
+else:
+    liquibased(options.host, options.tag)
+
 
 
 
