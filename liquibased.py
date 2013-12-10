1417798d1ef6aa367b84a2e3cf0db52a032347be
Brad Wallace <swellguy@gmail.com>
2013-12-09 16:28:00 -0800

    working arguments

--- liquibased.py
+++ liquibased.py
@@ -1,4 +1,4 @@
-  GNU nano 2.0.9                                                       File: liquibased.py
+   GNU nano 2.0.9                                                       File: liquibased.py
 
 #!/usr/bin/python
 # Calls liquibase against a target database
@@ -6,39 +6,43 @@
 # BMW 12/9/2013
 #java -jar liquibase.jar --classpath=/home/liquibase/certain/travis/lib/postgresql-9.3-1100.jdbc4.jar --driver=org.postgresql.Driver  --changeLogFile=/home/liquibase/certain/travis/certain/changelog.xml  -$
 #
-# Use:  liquibased(host, tag)
+# Use:  liquibased(host, tag, database)
 #
-from subprocess import call
+import subprocess
 import sys
+from optparse import OptionParser
 
+#declarations
 java_path = '/home/liquibase/certain/travis/lib/'
-dbname = 'certain'
 db_driver = '--classpath=/home/liquibase/certain/travis/lib/postgresql-9.3-1100.jdbc4.jar'
 source_path = '--changeLogFile=/home/liquibase/certain/travis/certain/changelog.xml'
 url='--url jdbc:postgresql://'
-url2='/' + dbname
 username='admin'
 password='3e3p0pCs'
 
+#handle arguments
+parser = OptionParser(usage="%prog [-o] [-t] [-d]", version="%prog .5")
+parser.add_option("-o", dest="host",
+                  help="host for liquibase")
+parser.add_option("-t",
+                  dest="tag",
+                  help="tag for liquibase")
+parser.add_option("-d",
+                  dest="dbname",
+                  help="destination database")
+
+(options, args) = parser.parse_args()
+
+url2 = '/' + options.dbname
 
 def liquibased(host, tag):
-#    try:
         target = url + host + url2
         command =  'java -jar ' + java_path +'liquibase.jar ' + db_driver + ' ' + source_path + ' ' + target + ' --username ' + username + ' --password ' + password + ' update'
-        print command
-        call(command, shell=True)
-        print target
+#        print command
+        subprocess.check_call(command, shell=True)
+        subprocess.check_call(command + ' tag ' + tag, shell=True)
 
-#    except:
-#        print 'Something Went Wrong!!'
-#        e = sys.exc_info()[0]
-#        print ( "<p>Error: %s</p>" % e )
-
-liquibased('postgresql01', 'brad')
-
-
-
-
-
+#print options.host + ' ' + options.tag
+liquibased(options.host, options.tag)
 
 
