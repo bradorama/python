2e42b882ae3314d852a1db3154160765a6d295dd
Brad Wallace <swellguy@gmail.com>
2013-12-09 16:15:37 -0800

    Working arguments

--- pgbackups.py
+++ pgbackups.py
@@ -9,8 +9,9 @@
 #
 import subprocess
 import sys
+from optparse import OptionParser
 
-tag = 'bradtest'
+#declarations
 java_path = '/home/liquibase/certain/travis/lib/'
 dbname = 'bradtest'
 db_driver = '--classpath=/home/liquibase/certain/travis/lib/postgresql-9.3-1100.jdbc4.jar'
@@ -20,15 +21,27 @@ url2='/' + dbname
 username='admin'
 password='3e3p0pCs'
 
+#handle arguments
+parser = OptionParser()
+parser.add_option("-o", dest="host",
+                  help="host for liquibase")
+parser.add_option("-t",
+                  dest="tag",
+                  help="tag for liquibase")
+
+(options, args) = parser.parse_args()
+
 
 def liquibased(host, tag):
         target = url + host + url2
         command =  'java -jar ' + java_path +'liquibase.jar ' + db_driver + ' ' + source_path + ' ' + target + ' --username ' + username + ' --password ' + password + ' update'
+        print command
         subprocess.check_call(command, shell=True)
         subprocess.check_call(command + ' tag ' + tag, shell=True)
 
+print options.host + ' ' + options.tag
+liquibased(options.host, options.tag)
 
 
-liquibased('postgresql01', 'brad')
 
 
