cc4463f448082ca219c79db58d2d9ab837ca045e
Brad Wallace <swellguy@gmail.com>
2013-12-17 10:40:14 -0800

    Added Safe/Unsafe modes.  Preliminary rollback support.

--- liquibased.py
+++ liquibased.py
@@ -15,7 +15,7 @@ import syslog
 #declarations
 java_path = '/home/liquibase/certain/travis/lib/'
 db_driver = '--classpath=/home/liquibase/certain/travis/lib/postgresql-9.3-1100.jdbc4.jar'
-source_path = '--changeLogFile=/home/liquibase/certain/travis/certain/changelog.xml' 
+source_path = '--changeLogFile=/home/liquibase/certain/travis/50x_db/db/certain/changelog.xml' 
 url='--url jdbc:postgresql://'
 url2= '/'
 username='admin'  
@@ -32,16 +32,26 @@ parser.add_option("-d",
 		  dest="dbname",
                   help="destination database (omit to process all databases)",
 		  default="alldatabases")
+parser.add_option("-m",
+		  dest="mode",
+                  help="Mode for deployment (S afe mode or U pdate)",
+		  default="S")
 		
 
 (options, args) = parser.parse_args()
 
 def liquibased(host, tag, database):
         target = url + host + url2 + database
-        command =  'java -jar ' + java_path +'liquibase.jar ' + db_driver + ' ' + source_path + ' ' + target + ' --username ' + username + ' --password ' + password + ' update' 
-        subprocess.check_call(command, shell=True)
+        command =  'java -jar ' + java_path +'liquibase.jar ' + db_driver + ' ' + source_path + ' ' + target + ' --username ' + username + ' --password ' + password 
+	if options.mode == 'U':
+		 exec_command = command + ' update'
+	elif options.mode == 'S':
+		 exec_command = command + ' updateSQL'
+	elif options.mode == 'R':
+		 command + ' rollback' + options.tag
+        subprocess.check_call(exec_command, shell=True)
 	syslog.syslog( host + '.' + database + ' updated Successfully.')
-        subprocess.check_call(command + ' tag ' + tag, shell=True)
+        #subprocess.check_call(command + ' tag ' + tag, shell=True)  Not Needed
 
 
 def getportals(host):
@@ -57,7 +67,7 @@ def getportals(host):
         
 
 if options.dbname == 'alldatabases':
-    syslog.syslog('Starting a Global LB Deployment on: ' + options.host)
+    syslog.syslog('Starting a Global LB Deployment on: ' + options.host + ' Mode:' + options.mode)
     rows = getportals(options.host)
     for row in rows:
 	    try:
@@ -67,12 +77,7 @@ if options.dbname == 'alldatabases':
 		syslog.syslog('***ERROR***  LB Deployment Failure on database: ' + row[0])
 else:
     print 'Processing: ' + options.dbname
-    syslog.syslog('Starting a direct LB Deployment on: ' + options.host + '.' + options.dbname)
+    syslog.syslog('Starting a direct LB Deployment on: ' + options.host + '.' + options.dbname + ' Mode:' + options.mode)
     liquibased(options.host, options.tag, options.dbname)
-
-
-
-
-
 
 
